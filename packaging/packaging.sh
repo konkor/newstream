4e1d963333e3f84883698b1e92354166ce52bb24
Konkor <kapa76@gmail.com>
2018-11-15 15:19:58 +0200

    Update move debian contol folder to packaging

--- packaging/packaging.sh
+++ packaging/packaging.sh
@@ -10,5 +10,5 @@ cd debs
 ln -s ../../$PKG_NAME-$VERSION.tar.xz newstream_$VERSION.orig.tar.xz
 tar xf newstream_$VERSION.orig.tar.xz
 cd $PKG_NAME-$VERSION
-cp -r ../../../debian/ .
+cp -r ../../debian/ .
 debuild -us -uc
